834d7c654fc01da690dac4818b5a9b52bb27399f
alexander vasilev <alexander.vasilev@protonmail.com>
2021-05-26 10:19:06 +0300

    added the meta box for expert's title and change the naming of some labels

--- custom-post-types/consultations.php
+++ custom-post-types/consultations.php
@@ -8,8 +8,8 @@ function bepf_add_experts()
 		'singular_name' => _x('Expert', 'post type singular name', 'bepf'),
 		'menu_name' => _x('Experts', 'admin menu', 'bepf'),
 		'name_admin_bar' => _x('Expert', 'add new on admin bar', 'bepf'),
-		'add_new' => _x('Add New', 'Expert', 'bepf'),
-		'add_new_item' => __('Add New', 'bepf'),
+		'add_new' => _x('Add New Expert', 'Expert', 'bepf'),
+		'add_new_item' => __('Add New Expert', 'bepf'),
 		'new_item' => __('New expert', 'bepf'),
 		'edit_item' => __('Edit expert', 'bepf'),
 		'view_item' => __('View expert', 'bepf'),
@@ -40,3 +40,54 @@ function bepf_add_experts()
 	);
 	register_post_type('expert', $args);
 }
+
+add_action('add_meta_boxes', 'wpse_add_custom_meta_box_2');
+function wpse_add_custom_meta_box_2()
+{
+	add_meta_box(
+		'lessons_number',
+		__('Professional title', 'bepf'),
+		'bepf_expert_title',
+		'expert',
+		'side',
+		'high'
+	);
+}
+
+//showing custom form fields
+function bepf_expert_title()
+{
+	global $post;
+	$title = get_post_meta($post->ID, 'expert_title', true);
+
+	?>
+	<label for="expert-title"></label>
+	<input id="expert-title" type="text" name=expert_title value="<?php echo $title ?>">
+	<?php
+
+}
+
+//now we are saving the data
+add_action('save_post', 'pp_add_lesson_number');
+add_action('new_to_publish', 'pp_add_lesson_number');
+function pp_add_lesson_number($post_id)
+{
+	if (isset($_POST['post_type']) && $_POST['post_type'] === 'expert') {
+		update_post_meta($post_id, 'expert_title', $_POST['expert_title']);
+	}
+}
+
+
+function change_default_title($title)
+{
+
+	$screen = get_current_screen();
+
+	if ($screen !== null && 'expert' === $screen->post_type) {
+		$title = __("Expert's name", 'bepf');
+	}
+
+	return $title;
+}
+
+add_filter('enter_title_here', 'change_default_title');
