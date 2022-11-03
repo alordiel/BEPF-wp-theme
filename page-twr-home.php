4c53fb169db5b92ddd93234bc753195a403437d5
alordiel <alexander.vasilev@protonmail.com>
2022-11-03 16:05:59 +0200

    hardcoidng the link

--- page-twr-home.php
+++ page-twr-home.php
@@ -54,7 +54,7 @@ Template Name: [ДСК] Начална
 								Дърво с корен 2022
 							</h2>
 
-						     <a style="visibility: visible" href="<?php echo get_permalink(6559); ?>">
+						     <a style="visibility: visible" href="https://bepf-bg.org/%d0%b4%d1%8a%d1%80%d0%b2%d0%be-%d1%81-%d0%ba%d0%be%d1%80%d0%b5%d0%bd/%d0%b3%d0%bb%d0%b0%d1%81%d1%83%d0%b2%d0%b0%d0%b9-%d0%b7%d0%b0-%d0%bb%d1%8e%d0%b1%d0%b8%d0%bc%d0%be-%d0%b1%d1%8a%d0%bb%d0%b3%d0%b0%d1%80%d1%81%d0%ba%d0%be-%d0%b4%d1%8a%d1%80%d0%b2%d0%be/">
 								 <img class="img-responsive" src="https://bepf-bg.org/bepf2015/wp-content/uploads/2017/09/btn-vote-twr.png" alt="Гласува за Дърво с корен">
 							 </a>
 							 <!--<h2>
