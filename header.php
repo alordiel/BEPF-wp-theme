e0a9ae0e74e7cd85b7819e49b42bc3dd9b92a7d6
alordiel <alexander.vasilev@protonmail.com>
2024-08-23 17:03:02 +0300

    second fixed link of header icon

--- header.php
+++ header.php
@@ -47,7 +47,11 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="<?php echo site_url()?>"></a>
+                <?php
+                $current_lang =  apply_filters( 'wpml_current_language', null );
+                $lang = $current_lang === 'en' ? 'en' : '';
+                ?>
+				<a class="navbar-brand" href="<?php echo site_url($lang)?>"></a>
 			</div>
 
 			<div class="collapse navbar-collapse navbar-responsive-collapse navbar-right">
